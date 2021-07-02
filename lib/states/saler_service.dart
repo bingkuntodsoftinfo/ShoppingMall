42bb60aeec6cdebc704adc411872c4a14556cb6e
Nong <nong@abc.com>
2021-07-02 11:00:52 +0700

    Create State

---
+++ lib/states/saler_service.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SalreService extends StatefulWidget {
+  const SalreService({ Key? key }) : super(key: key);
+
+  @override
+  _SalreServiceState createState() => _SalreServiceState();
+}
+
+class _SalreServiceState extends State<SalreService> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
---
+++ lib/utility/my_constant.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MyConstant {
+  //General
+  static String appName = 'Shopping Name';
+
+  //Route
+  static String routeAuthen = '/authen';
+  static String routeCreateAccount = '/createAccount';
+  static String routeByerService = 'buyerService';
+  static String routeSelerService = 'salerService';
+  static String routeRiderService = 'riderService';
+}
